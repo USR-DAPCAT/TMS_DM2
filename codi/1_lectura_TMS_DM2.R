e98fc4e4032b6b6e2e0d63e3d552e4ca535b1a10
jrealgatius <jordireal@gmail.com>
2021-07-15 11:02:41 +0200

    Inici de projecte

---
+++ codi/1_lectura_TMS_DM2.R
@@ -0,0 +1,18 @@
+
+
+
+###### lectura de dades 
+
+link_source<-paste0("https://github.com/jrealgatius/Stat_codis/blob/master/funcions_propies.R","?raw=T")
+devtools::source_url(link_source)
+
+
+
+# list.files("dades")
+# 
+# generar_mostra_fitxers(directori = "dades",
+#                        fitxer_poblacio="pacients.txt",
+#                        mida_mostra=5000,
+#                        prefix="",
+#                        directori_test="mostra",idp="IDP")
+
